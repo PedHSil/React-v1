95fb16648422f334585d87db90ff4fc441b47405
Pedro <pedroh422silva@gmail.com>
2025-11-17 09:25:58 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { Features } from "lightningcss"
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import Features from "./components/Features.jsx"
+import Pricing from "./components/Pricing.jsx"
+import Testimonials from "./components/Testimonials.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
